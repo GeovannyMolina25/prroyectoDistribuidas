38db3bb0fb265f5464ab91a93ed7aab158f9b0f2
Nelson Molina <geovannysangucho25@gmail.com>
2024-02-09 12:00:17 -0500

    doraemn3

--- src/controllers/controlCombustible.controller.js
+++ src/controllers/controlCombustible.controller.js
@@ -108,7 +108,7 @@ const getVehiConductor = async (req, res) => {
 
         if (response.rows && response.rows.length > 0) {
             const conductores = response.rows[0]; 
-            console.log(typeof conductores)
+            console.log(conductores)
             conductoresNew  = conductores.split(",")
             res.json(conductoresNew);
         } else {
