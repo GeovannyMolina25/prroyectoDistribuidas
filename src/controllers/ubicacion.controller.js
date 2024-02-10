c0ff42d7790a27fd7f75fe03b48f3cdca28fce6b
aaquinonez01 <aaquinonez280801@gmail.com>
2024-02-09 21:47:57 -0500

    ubicaciones sin repetir

--- src/controllers/ubicacion.controller.js
+++ src/controllers/ubicacion.controller.js
@@ -1,21 +1,25 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    host : process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-const getUbicacion = async(req,res) => {
-    const response = await pool.query('Select * from ubicacion');
-    res.status(200).json(response.rows);
-}
+const getUbicacion = async (req, res) => {
+  const response = await pool.query("Select * from ubicacion");
+  res.status(200).json(response.rows);
+};
 
-module.exports={
-    getUbicacion
-}
+const getUbicacionSinRepetirOrigen = async (req, res) => {
+  const response = await pool.query("SELECT DISTINCT origen FROM ubicacion");
+  res.status(200).json(response.rows);
+};
 
-
+module.exports = {
+  getUbicacion,
+  getUbicacionSinRepetirOrigen,
+};
